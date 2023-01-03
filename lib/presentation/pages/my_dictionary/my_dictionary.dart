4bd14b12ba47425afb7db4132c272b35f1019209
Minh Dang Le <58287394+minhdang2803@users.noreply.github.com>
2023-01-03 21:09:30 +0700

    Start Capstone project

--- lib/presentation/pages/my_dictionary/my_dictionary.dart
+++ lib/presentation/pages/my_dictionary/my_dictionary.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../bloc/vocab/vocab_cubit.dart';
 import '../../theme/app_color.dart';
-import '../../widgets/cvn_app_bar.dart';
 import 'vocab_item.dart';
 
 class MyDictionaryPage extends StatelessWidget {
